11f790f6aca6df10b16e39e2aa2bafb14436059b
Vinuthna-Reddy <76032736+Vinuthna-Reddy@users.noreply.github.com>
2020-12-16 10:48:56 +0530

    Create Class2.cs

---
+++ gitdemoproj/gitdemoproj/Class2.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitdemoproj
+{
+    class Class2
+    {
+        public void sayHi()
+        {
+            Console.WriteLine("Hi");
+        }
+       
+    }
+}
