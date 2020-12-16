da23282dfa666db5d4889d98b85d58c4b673a106
Vinuthna Reddy <vinuthna.malreddy@lntinfotech.com>
2020-12-16 09:39:41 +0530

    Update few lines

--- gitdemoproj/gitdemoproj/Program.cs
+++ gitdemoproj/gitdemoproj/Program.cs
@@ -11,6 +11,7 @@ namespace gitdemoproj
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("adding a new line");
         }
     }
 }
